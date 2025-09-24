7ec08d0fceb973deea14556609defd18cc528615
vishal <bhuvnesh@autovyn.in>
2025-09-24 11:10:22 +0530

    api

--- ui/src/api/index.js
+++ ui/src/api/index.js
@@ -1,36 +1,36 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
-      ? `${location.protocol}//${location.hostname}:${process.env.VUE_APP_SERVER_PORT}/api`
-      : `${location.origin}/api`,
-});
+      ? 'https://cameraui-c3rk.onrender.com/api'
+      : 'https://cameraui-c3rk.onrender.com/api'
+})
 
 api.interceptors.request.use(
   (config) => {
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem('user'))
 
     if (user && user.access_token) {
-      config.headers['Authorization'] = `Bearer ${user.access_token}`;
+      config.headers['Authorization'] = `Bearer ${user.access_token}`
     }
 
-    return Promise.resolve(config);
+    return Promise.resolve(config)
   },
 
   (error) => {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 api.interceptors.response.use(
   (response) => {
-    return Promise.resolve(response);
+    return Promise.resolve(response)
   },
 
   (error) => {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
-export default api;
+export default api
